b358eff3a32795f2a5d4ce93181d6ffeed7dc396
Francisco Delgado <francisco.delgado@ioconnectservices.com>
2023-09-29 19:10:31 -0600

    - creando componente layout para app

---
+++ src/components/layout/index.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import NavBar from '../nav-bar'
+
+const Layout = ({ children }) => {
+  return (
+    <>
+      <NavBar />
+      {children}
+    </>
+  )
+}
+
+export default Layout
